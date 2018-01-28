a83c50bd99a7b304faaccf923496dc54a71e45e7
@kirilloleynik <kirill.olejnik@gmail.com>
2018-01-28 20:23:37 +0200

    Update template method example

--- template/base_form.rb
+++ template/base_form.rb
@@ -1,15 +1,15 @@
+# Abstract base form
 class Base
+  # Adds required behavior to inherited classes
   include Virtus.model
   include ActiveModel::Model
 
-  def persisted?
-    false
-  end
-
+  # Requires all forms to implement 'record' hook
   def record
     raise NotImplementedError
   end
 
+  # Provides unified saving algorithm for all forms
   def save
     return false unless valid?
     persist!
@@ -18,6 +18,7 @@ class Base
 
   private
 
+  # Requires all forms to implement 'persist!' hook
   def persist!
     raise NotImplementedError
   end
--- template/create_form.rb
+++ template/create_form.rb
@@ -1,24 +1,24 @@
+# Concrete form class inherited from Abstract class
 class Create < Base
-  attribute :attribute_one,   String
-  attribute :attribute_two,   String
-  attribute :attribute_three, String
+  # Expects to receive two attributes type of String
+  attribute :attribute_one, String
+  attribute :attribute_two, String
 
-  validates :attribute_one,
-            :attribute_two,
-            :attribute_three,
-            presence: true
+  # Validates attributes to be present
+  validates :attribute_one, :attribute_two, presence: true
 
+  # Defines 'record' hook which is required by abstract class
   def record
     @record ||= SomeModel.new
   end
 
   private
 
+  # Defines 'persist!' hook which is required by abstract class
   def persist!
     record.assign_attributes(
       attribute_one: attribute_one,
-      attribute_two: attribute_two,
-      attribute_three: attribute_three
+      attribute_two: attribute_two
     )
     record.save!
   end
