dad9f1d07954b0ef4845899ffa119cf8b119ffe5
kirill-oleynik <kirill.olejnik@gmail.com>
2018-01-31 00:37:14 +0200

    Update adapter example

--- adapter/bcrypt_adapter.rb
+++ adapter/bcrypt_adapter.rb
@@ -1,11 +1,19 @@
+# Adapter for BCrypt
 class BcryptAdapter
   COST = 12
 
+  # BCrypt provides interface to generate salt and hash secret
+  # Adapter combines these ablities to single method 'encode'
+  # and provides ability to influence on result with arguments
   def encode(secret, salt: nil, cost: COST)
     salt ||= BCrypt::Engine.generate_salt(cost)
     BCrypt::Engine.hash_secret(secret, salt)
   end
 
+  # BCrypt does not provide all required interface
+  # for secure compare pure secret with secret hash
+  # So adapter uses existing BCrypt interface
+  # and completes it with ActiveSupport utils
   def compare(secret:, secret_hash:)
     salt = BCrypt::Password.new(secret_hash).salt
     hash = encode(secret, salt: salt)
--- adapter/jwt_adapter.rb
+++ adapter/jwt_adapter.rb
@@ -1,3 +1,8 @@
+# JWT porvides 'encode' and 'decode' methods,
+# but they require multiple arguments: payload, SECRET, ALGORITHM
+# Adapter modifies each method's behavior
+# and takes care about all common arguments (SECRET, ALGORITHM)
+# so we are required to provide only payload
 class JwtAdapter
   SECRET = ENV['SECRET_KEY_BASE']
   ALGORITHM = 'HS512'.freeze
